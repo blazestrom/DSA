a19c15330d9c33790b974d4e8b9c88a6307dfb57
Piyush Awadhiya <166833248+blazestrom@users.noreply.github.com>
2026-05-23 09:34:41 +0530

    Time: 644 ms (27.06%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,23 +1,16 @@
-SELECT 
-    p1.product_id,
-    IFNULL(p2.new_price,10) AS price
-FROM
-(
-    SELECT DISTINCT product_id
-    FROM Products
-) p1
-LEFT JOIN
-(
-    SELECT product_id,new_price
-    FROM Products
-    WHERE (product_id,change_date) IN
-    (
-        SELECT 
-            product_id,
-            MAX(change_date)
-        FROM Products
-        WHERE change_date <= '2019-08-16'
-        GROUP BY product_id
-    )
-) p2
-ON p1.product_id = p2.product_id;
+select distinct product_id , 10 as price
+ from products 
+ where product_id not in(
+    select product_id 
+    from products 
+    where  change_date<='2019-08-16'
+ )
+ 
+union
+select product_id , new_price as price 
+from products
+where (product_id ,change_date)in 
+(select product_id,max(change_date)
+from products
+where change_date<='2019-08-16'
+group by product_id)
