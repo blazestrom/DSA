21f352a6baca95ac7db7e60b2037bb6f5ee5602e
Piyush Awadhiya <166833248+blazestrom@users.noreply.github.com>
2026-06-29 19:33:12 +0530

    Time: 0 ms (100%), Space: 42.2 MB (35.27%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+     int a=0;
+     int b=1;
+     int c=a+b;
+     for(int i=0;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+     }   
+     return c;
+    
+        
+     
+      
+    }
+}
