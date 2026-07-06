7a56ea920f9bd94f96c07245a15987c2620a66da
Piyush Awadhiya <166833248+blazestrom@users.noreply.github.com>
2026-07-06 21:36:41 +0530

    Time: 16 ms (34.64%), Space: 48.5 MB (47.8%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack <Integer> stack= new Stack<>();
+    
+        int []arr = new int[nums.length];
+        for(int i=2*nums.length-1;i>=0;i--){
+            while(!stack.isEmpty()&& stack.peek()<=nums[i%nums.length]){
+                 stack.pop();
+            }
+            if(i<nums.length){
+                arr[i]=stack.isEmpty()?-1:stack.peek();
+            }
+            stack.push(nums[i%nums.length]);
+        }
+        return arr;
+    }
+}
